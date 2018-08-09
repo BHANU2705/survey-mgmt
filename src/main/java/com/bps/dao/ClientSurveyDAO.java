db8668244dd211e3c4be9a24111a6be495e6fbf2
Bhanu Pratap Sharan <bhanu.pratap.sharan@sap.com>
2018-08-09 19:52:19 +0530

    isResponded
    Change-Id: I2506074fc71c04f918864ab6e68735ed879e7adf
    Signed-off-by: Bhanu Pratap Sharan <bhanu.pratap.sharan@sap.com>

--- src/main/java/com/bps/dao/ClientSurveyDAO.java
+++ src/main/java/com/bps/dao/ClientSurveyDAO.java
@@ -1,8 +1,11 @@
 package com.bps.dao;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Session;
 import org.hibernate.query.Query;
@@ -68,8 +71,38 @@ public class ClientSurveyDAO extends DAO implements IBaseDAO {
 	}
 
 	private void setIsResponded(Session session, List<ClientSurvey> clientSurveys) {
+		final String pattern = "''{0}'',";
+		StringBuilder builder = new StringBuilder();
 		for (ClientSurvey clientSurvey : clientSurveys) {
-			clientSurvey.setResponded(false);
+			if (clientSurvey != null && clientSurvey.getId() != null && !clientSurvey.getId().isEmpty()) {
+				builder.append(MessageFormat.format(pattern, clientSurvey.getId()));
+			}
+		}
+		String str = builder.toString();
+		str = str.substring(0, str.length() - 1);
+		String queryString = "SELECT s.surveyId, count(*) as count " + "FROM SurveyResponse s WHERE s.userId = '"
+				+ super.getUserEmail() + "' and s.surveyId IN (" + str + ") group by surveyId";
+		Query<?> query = session.createQuery(queryString);
+		query.setReadOnly(true);
+		List<?> data = query.getResultList();
+		Map<String, Boolean> map = new HashMap<>();
+		if (data != null && !data.isEmpty()) {
+			Iterator<?> it = data.iterator();
+			while (it.hasNext()) {
+				Object[] list = (Object[]) it.next();
+				String surveyId = (String) list[0];
+				Long count = (Long) list[1];
+				Boolean isResponded = (count > 0) ? true : false;
+				map.put(surveyId, isResponded);
+			}
+		}
+
+		for (ClientSurvey clientSurvey : clientSurveys) {
+			if (map.containsKey(clientSurvey.getId())) {
+				clientSurvey.setResponded(map.get(clientSurvey.getId()));
+			} else {
+				clientSurvey.setResponded(false);
+			}
 		}
 	}
 
